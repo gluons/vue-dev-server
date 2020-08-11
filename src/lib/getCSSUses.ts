0e1e04bff5b99580f87f9861e6d70d85c9bd9037
Saran Tanpituckpong <sarunta@gmail.com>
2020-08-11 11:38:27 +0700

    ✨ Add `getCSSUses`

---
+++ src/lib/getCSSUses.ts
@@ -0,0 +1,27 @@
+import { RuleSetUseItem } from 'webpack';
+
+/**
+ * Get webpack rules' CSS `use`.
+ *
+ * @param {number} [importLoaders=1] `css-loader`'s `importLoaders`
+ */
+export default function getCSSUses(importLoaders = 1): RuleSetUseItem[] {
+	const cssUses: RuleSetUseItem[] = [
+		'vue-style-loader',
+		{
+			loader: 'css-loader',
+			options: {
+				importLoaders,
+				sourceMap: true
+			}
+		},
+		{
+			loader: 'postcss-loader',
+			options: {
+				sourceMap: true
+			}
+		}
+	];
+
+	return cssUses;
+}
