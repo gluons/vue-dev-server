6814d51cb4293c887215d587298ac3ed6353bf5a
Saran Tanpituckpong <sarunta@gmail.com>
2020-08-15 19:35:42 +0700

    👽 Fix style won't load due to `vue-style-loader` & `css-loader` v4 compatibility
    See vuejs/vue-style-loader#46

--- src/lib/getCSSUses.ts
+++ src/lib/getCSSUses.ts
@@ -7,12 +7,20 @@ import { RuleSetUseItem } from 'webpack';
  * @param {number} [importLoaders=1] `css-loader`'s `importLoaders`
  */
 export default function getCSSUses(importLoaders = 1): RuleSetUseItem[] {
+	/*
+	 * To make `vue-style-loader` works with `css-loader` v4,
+	 * we need to disable `esModule`.
+	 *
+	 * See https://github.com/vuejs/vue-style-loader/issues/46
+	 */
+
 	const cssUses: RuleSetUseItem[] = [
 		'vue-style-loader',
 		{
 			loader: 'css-loader',
 			options: {
 				importLoaders,
+				esModule: false,
 				sourceMap: true
 			}
 		},
